7bdd830fd835575033df38254c60249bade44e5b
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-03-29 01:50:38 +0200

    Add xentrace-parser.h comments

--- src/xentrace-parser.h
+++ src/xentrace-parser.h
@@ -30,42 +30,52 @@
 typedef struct __xentrace_parser *xentrace_parser;
 
 /**
- * 
+ * Create a new instance based on the 
+ * file path passed as an argument.
+ * Returns NULL on error.
  */
 xentrace_parser xtp_init(const char*);
 
 /**
- * 
+ * Performs trace parsing.
+ * If a trace file is damaged, it will 
+ * read all events until a read error occurs.
+ * If executed a second time, it returns 
+ * only the number of previously read elements.
+ * Returns zero on error.
  */
 uint32_t xtp_execute(xentrace_parser);
 
 /**
- * 
+ * Returns the CPUs count of the trace.
  */
 uint16_t xtp_cpus_count(xentrace_parser);
 
 /**
- * 
+ * Returns the events count of the trace.
  */
 uint32_t xtp_events_count(xentrace_parser);
 
 /**
- * 
+ * Returns the event at position X of the list.
+ * Returns NULL on error.
  */
 xt_event *xtp_get_event(xentrace_parser, uint32_t);
 
 /**
- * 
+ * Returns the next event in the list,
+ * based on the position of the iterator.
+ * Returns NULL on error/end-of-list.
  */
 xt_event *xtp_next_event(xentrace_parser);
 
 /**
- * 
+ * Resets the list iterator.
  */
 void xtp_reset_iter(xentrace_parser);
 
 /**
- * 
+ * Frees up an instance.
  */
 void xtp_free(xentrace_parser);
 
